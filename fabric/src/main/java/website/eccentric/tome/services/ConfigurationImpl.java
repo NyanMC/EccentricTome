008960c726483e6e8ea5757a82a81b2bfc64a9a3
EccentricVamp <EccentricVamp@gmail.com>
2022-05-08 14:12:26 -0500

    Fix fabric configuration placeholder

--- fabric/src/main/java/website/eccentric/tome/services/ConfigurationImpl.java
+++ fabric/src/main/java/website/eccentric/tome/services/ConfigurationImpl.java
@@ -1,19 +1,91 @@
 package website.eccentric.tome.services;
 
-import java.util.HashMap;
 import java.util.List;
-
-import website.eccentric.tome.CommonConfiguration;
+import java.util.Map;
 
 public class ConfigurationImpl implements Configuration {
 
-    private static boolean allItems;
-    private static boolean disableOverlay;
-    private static List<? extends String> items;
-    private static List<? extends String> names;
-    private static HashMap<String, String> aliases;
-    private static List<? extends String> exclude;
-    private static List<? extends String> excludeItems;
+    private static boolean allItems = false;
+    private static boolean disableOverlay = false;
+
+    private static List<? extends String> items = List.of(
+        "tconstruct:materials_and_you",
+        "tconstruct:puny_smelting",
+        "tconstruct:mighty_smelting",
+        "tconstruct:fantastic_foundry",
+        "tconstruct:tinkers_gadgetry",
+        "integrateddynamics:on_the_dynamics_of_integration",
+        "cookingforblockheads:no_filter_edition",
+        "alexsmobs:animal_dictionary",
+        "occultism:dictionary_of_spirits",
+        "theoneprobe:probenote",
+        "compactmachines:personal_shrinking_device"
+    );
+
+    private static List<? extends String> names = List.of(
+        "book",
+        "tome",
+        "lexicon",
+        "nomicon",
+        "manual",
+        "knowledge",
+        "pedia",
+        "compendium",
+        "guide",
+        "codex",
+        "journal",
+        "enchiridion"
+    );
+
+    private static Map<String, String> aliases = Map.of(
+        "thermalexpansion", "thermalfoundation",
+        "thermaldynamics", "thermalfoundation",
+        "thermalcultivation", "thermalfoundation",
+        "redstonearsenal", "thermalfoundation",
+        "rftoolsdim", "rftools",
+        "rftoolspower", "rftools",
+        "rftoolscontrol", "rftools",
+        "xnet", "rftools"
+    );
+
+    private static List<? extends String> exclude = List.of();
+    
+    private static List<? extends String> excludeItems = List.of(
+        "apotheosis:boots_tome",
+        "apotheosis:bow_tome",
+        "apotheosis:chestplate_tome",
+        "apotheosis:fishing_tome",
+        "apotheosis:helmet_tome",
+        "apotheosis:leggings_tome",
+        "apotheosis:other_tome",
+        "apotheosis:pickaxe_tome",
+        "apotheosis:scrap_tome",
+        "apotheosis:weapon_tome",
+        "ars_nouveau:annotated_codex",
+        "darkutils:tome_enchanting",
+        "darkutils:tome_illager",
+        "darkutils:tome_pigpen",
+        "darkutils:tome_runelic",
+        "darkutils:tome_sga",
+        "darkutils:tome_shadows",
+        "minecolonies:ancienttome",
+        "occultism:book_of_binding_afrit",
+        "occultism:book_of_binding_bound_afrit",
+        "occultism:book_of_binding_bound_djinni",
+        "occultism:book_of_binding_bound_foliot",
+        "occultism:book_of_binding_bound_marid",
+        "occultism:book_of_binding_djinni",
+        "occultism:book_of_binding_foliot",
+        "occultism:book_of_binding_marid",
+        "occultism:book_of_calling_djinni_manage_machine",
+        "occultism:book_of_calling_foliot_cleaner",
+        "occultism:book_of_calling_foliot_lumberjack",
+        "occultism:book_of_calling_foliot_transport_items",
+        "tombstone:book_of_disenchantment",
+        "tombstone:book_of_recycling",
+        "tombstone:book_of_repairing",
+        "tombstone:book_of_magic_impregnation"
+    );
         
     public boolean allItems() {
         return allItems;
@@ -31,7 +103,7 @@ public class ConfigurationImpl implements Configuration {
         return names;
     }
 
-    public HashMap<String, String> aliases() {
+    public Map<String, String> aliases() {
         return aliases;
     }
 
@@ -45,18 +117,6 @@ public class ConfigurationImpl implements Configuration {
     
 
     public void refresh() {
-        allItems = CommonConfiguration.ALL_ITEMS.get();
-        disableOverlay = CommonConfiguration.DISABLE_OVERLAY.get();
-        items = CommonConfiguration.ITEMS.get();
-        names = CommonConfiguration.NAMES.get();
-
-        aliases = new HashMap<String, String>();
-        for (var alias : CommonConfiguration.ALIASES.get()) {
-            var tokens = alias.split("=");
-            aliases.put(tokens[0], tokens[1]);
-        }
-
-        exclude = CommonConfiguration.EXCLUDE.get();
-        excludeItems = CommonConfiguration.EXCLUDE_ITEMS.get();
+        // TODO: write this
     }
 }
