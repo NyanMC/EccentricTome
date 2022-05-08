160aee107bd8fb11da54e8192b607be7edfa668c
EccentricVamp <EccentricVamp@gmail.com>
2022-05-08 14:24:10 -0500

    Add minecraft mixin

---
+++ fabric/src/main/java/website/eccentric/tome/mixins/MinecraftMixin.java
@@ -0,0 +1,34 @@
+package website.eccentric.tome.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.util.thread.ReentrantBlockableEventLoop;
+import website.eccentric.tome.TomeItem;
+import website.eccentric.tome.network.RevertHandler;
+
+@Mixin(Minecraft.class)
+public abstract class MinecraftMixin extends ReentrantBlockableEventLoop<Runnable> {
+
+    @Shadow
+    public LocalPlayer player;
+
+    public MinecraftMixin(String string) {
+        super(string);
+    }
+
+    @Inject(method = "startAttack", at = @At(value = "INVOKE", target = "resetAttackStrengthTicker"))
+    public void onMiss(CallbackInfoReturnable<Boolean> cir) {
+        var stack = this.player.getMainHandItem();
+        if (TomeItem.isTome(stack) && !(stack.getItem() instanceof TomeItem)) {
+            ClientPlayNetworking.send(RevertHandler.LOCATION, PacketByteBufs.empty());
+        }
+    }
+}
